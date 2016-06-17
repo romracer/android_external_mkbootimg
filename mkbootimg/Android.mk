647abc6f2c16d3c43de5910b48abee844b4170e6
Scott <romracer@gmail.com>
2016-06-17 14:36:21 -0500

    Remove duplicate definition

--- mkbootimg/Android.mk
+++ mkbootimg/Android.mk
@@ -1,16 +1,6 @@
 
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := mkbootimg.c
-LOCAL_STATIC_LIBRARIES := libmincrypt
-LOCAL_CFLAGS := -Werror
-
-LOCAL_MODULE := mkbootimg
-
-include $(BUILD_HOST_EXECUTABLE)
-
-include $(CLEAR_VARS)
 LOCAL_SRC_FILES := unpackbootimg.c
 LOCAL_MODULE := unpackbootimg
 include $(BUILD_HOST_EXECUTABLE)
